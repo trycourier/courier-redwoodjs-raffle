6f1753b1c2651be5345743b9457c77b499906f17
Carter Rabasa <carter.rabasa@gmail.com>
2023-09-25 16:43:52 -0700

    sms webhook + Courier modules

--- api/src/functions/smsWebhook/smsWebhook.js
+++ api/src/functions/smsWebhook/smsWebhook.js
@@ -1,31 +1,80 @@
+import { createHash } from 'crypto'
+
+import { CourierClient } from '@trycourier/courier'
+
 import { logger } from 'src/lib/logger'
 
 /**
- * The handler function is your code that processes http request events.
- * You can use return and throw to send a response or error, respectively.
- *
- * Important: When deployed, a custom serverless function is an open API endpoint and
- * is your responsibility to secure appropriately.
- *
- * @see {@link https://redwoodjs.com/docs/serverless-functions#security-considerations|Serverless Function Considerations}
- * in the RedwoodJS documentation for more information.
+ * processes inbound Twilio webhook requests
  *
  * @typedef { import('aws-lambda').APIGatewayEvent } APIGatewayEvent
  * @typedef { import('aws-lambda').Context } Context
  * @param { APIGatewayEvent } event - an object which contains information from the invoker.
- * @param { Context } context - contains information about the invocation,
- * function, and execution environment.
+ * @param { Context } context - contains information about the invocation, function, and execution environment.
  */
 export const handler = async (event, _context) => {
   logger.info(`${event.httpMethod} ${event.path}: smsWebhook function`)
 
+  // get values from the POST
+  const params = new URLSearchParams(event.body)
+  const phoneNumber = params.get('From')
+  const name = params.get('Body')
+  // create a unique id for this user based on their phone number
+  const userId = createHash('sha3-256').update(phoneNumber).digest('hex')
+  // create a Profile for this user
+  const courier = CourierClient()
+  await courier.mergeProfile({
+    recipientId: userId,
+    profile: {
+      phone_number: phoneNumber,
+      phone_number_verified: true,
+      name,
+    },
+  })
+  // Add the user to our List
+  await courier.lists.subscribe(process.env.COURIER_LIST_ID, userId)
+  // Send an in-app toast and Inbox notification to this website
+  await courier.send({
+    message: {
+      to: {
+        user_id: process.env.NEXT_PUBLIC_COURIER_USER,
+      },
+      content: {
+        body: '{{ name }} has entered the raffle!',
+      },
+      routing: {
+        method: 'single',
+        channels: ['inbox'],
+      },
+      data: {
+        name,
+      },
+    },
+  })
+  // Send a reply to the user letting them know we've recieved their entry
+  await courier.send({
+    message: {
+      to: {
+        user_id: userId,
+      },
+      content: {
+        body: 'Thank you for entering the Courier raffle! Check out our developer docs to learn more about how Courier works: https://courier.com/docs',
+      },
+      routing: {
+        method: 'single',
+        channels: ['sms'],
+      },
+    },
+  })
+
+  // return a 200 OK to Twilio
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      data: 'smsWebhook function',
+      data: `OK`,
     }),
   }
 }
--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,7 @@
+import { Inbox } from '@trycourier/react-inbox'
+import { CourierProvider } from '@trycourier/react-provider'
+import { Toast } from '@trycourier/react-toast'
+
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
@@ -5,15 +9,27 @@ const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.jsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <section style={{ display: 'flex' }}>
+        <article style={{ width: '400px' }}>
+          <h1>HomePage</h1>
+          <p>
+            Find me in <code>./web/src/pages/HomePage/HomePage.jsx</code>
+          </p>
+          <p>
+            My default route is named <code>home</code>, link to me with `
+            <Link to={routes.home()}>Home</Link>`
+          </p>
+        </article>
+        <article>
+          <CourierProvider
+            userId={process.env.PUBLIC_COURIER_USER}
+            clientKey={process.env.PUBLIC_COURIER_CLIENT_KEY}
+          >
+            <Inbox />
+            <Toast />
+          </CourierProvider>
+        </article>
+      </section>
     </>
   )
 }

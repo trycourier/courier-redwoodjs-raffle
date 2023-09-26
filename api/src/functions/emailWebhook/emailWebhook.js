2cfed1f1e44a955d5fd7878a7a8d3fa47b494201
Carter Rabasa <carter.rabasa@gmail.com>
2023-09-26 09:15:15 -0700

    secured webhooks

--- api/src/functions/emailWebhook/emailWebhook.js
+++ api/src/functions/emailWebhook/emailWebhook.js
@@ -20,74 +20,87 @@ import { logger } from 'src/lib/logger'
 export const handler = async (event, _context) => {
   logger.info(`${event.httpMethod} ${event.path}: emailWebhook function`)
 
-  // TODO: Lock down this webhook by authenticating the request!
+  const secret = event?.queryStringParameters?.secret
 
-  // get from email address and subject from Postmark webhook payload
-  const params = JSON.parse(event.body)
-  const fromEmail = params.From
-  const name = params.Subject
-  // create a unique id for this user based on their email address
-  const userId = createHash('sha3-256').update(fromEmail).digest('hex')
-  // create a Profile for this user
-  const courier = CourierClient()
-  await courier.mergeProfile({
-    recipientId: userId,
-    profile: {
-      email: fromEmail,
-      email_verified: true,
-      name,
-    },
-  })
-  // Add the user to our List
-  await courier.lists.subscribe(process.env.COURIER_LIST_ID, userId)
-  // Send an in-app toast and Inbox notification to this website
-  await courier.send({
-    message: {
-      to: {
-        user_id: process.env.PUBLIC_COURIER_USER,
+  if (secret !== process.env.WEBHOOK_SECRET) {
+    // return a 403
+    return {
+      statusCode: 403,
+      headers: {
+        'Content-Type': 'application/json',
       },
-      content: {
-        body: '{{ name }} has entered the raffle!',
-      },
-      routing: {
-        method: 'single',
-        channels: ['inbox'],
-      },
-      data: {
+      body: JSON.stringify({
+        status: 'Not Authorized',
+      }),
+    }
+  } else {
+    // get from email address and subject from Postmark webhook payload
+    const params = JSON.parse(event.body)
+    const fromEmail = params.From
+    const name = params.Subject
+    // create a unique id for this user based on their email address
+    const userId = createHash('sha3-256').update(fromEmail).digest('hex')
+    // create a Profile for this user
+    const courier = CourierClient()
+    await courier.mergeProfile({
+      recipientId: userId,
+      profile: {
+        email: fromEmail,
+        email_verified: true,
         name,
       },
-    },
-  })
-  // Send a reply to the user letting them know we've recieved their entry
-  await courier.send({
-    message: {
-      to: {
-        user_id: userId,
-      },
-      content: {
-        title: 'Your raffle entry has been recieved! 👍',
-        body: 'Thank you for entering the Courier raffle! Check out our developer docs to learn more about how Courier works: [courier.com/docs](https://bit.ly/3JoXobz)',
-      },
-      channels: {
-        email: {
-          providers: ['postmark'],
+    })
+    // Add the user to our List
+    await courier.lists.subscribe(process.env.COURIER_LIST_ID, userId)
+    // Send an in-app toast and Inbox notification to this website
+    await courier.send({
+      message: {
+        to: {
+          user_id: process.env.PUBLIC_COURIER_USER,
+        },
+        content: {
+          body: '{{ name }} has entered the raffle!',
+        },
+        routing: {
+          method: 'single',
+          channels: ['inbox'],
+        },
+        data: {
+          name,
         },
       },
-      routing: {
-        method: 'single',
-        channels: ['email'],
+    })
+    // Send a reply to the user letting them know we've recieved their entry
+    await courier.send({
+      message: {
+        to: {
+          user_id: userId,
+        },
+        content: {
+          title: 'Your raffle entry has been recieved! 👍',
+          body: 'Thank you for entering the Courier raffle! Check out our developer docs to learn more about how Courier works: [courier.com/docs](https://bit.ly/3JoXobz)',
+        },
+        channels: {
+          email: {
+            providers: ['postmark'],
+          },
+        },
+        routing: {
+          method: 'single',
+          channels: ['email'],
+        },
       },
-    },
-  })
+    })
 
-  // return a 200 OK to Postmark
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      status: 'OK',
-    }),
+    // return a 200 OK to Postmark
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        status: 'OK',
+      }),
+    }
   }
 }
--- api/src/functions/smsWebhook/smsWebhook.js
+++ api/src/functions/smsWebhook/smsWebhook.js
@@ -15,68 +15,81 @@ import { logger } from 'src/lib/logger'
 export const handler = async (event, _context) => {
   logger.info(`${event.httpMethod} ${event.path}: smsWebhook function`)
 
-  // TODO: Lock down this webhook by authenticating the request!
+  const secret = event?.queryStringParameters?.secret
 
-  // get values from the POST
-  const params = new URLSearchParams(event.body)
-  const phoneNumber = params.get('From')
-  const name = params.get('Body')
-  // create a unique id for this user based on their phone number
-  const userId = createHash('sha3-256').update(phoneNumber).digest('hex')
-  // create a Profile for this user
-  const courier = CourierClient()
-  await courier.mergeProfile({
-    recipientId: userId,
-    profile: {
-      phone_number: phoneNumber,
-      phone_number_verified: true,
-      name,
-    },
-  })
-  // Add the user to our List
-  await courier.lists.subscribe(process.env.COURIER_LIST_ID, userId)
-  // Send an in-app toast and Inbox notification to this website
-  await courier.send({
-    message: {
-      to: {
-        user_id: process.env.PUBLIC_COURIER_USER,
+  if (secret !== process.env.WEBHOOK_SECRET) {
+    // return a 403
+    return {
+      statusCode: 403,
+      headers: {
+        'Content-Type': 'application/json',
       },
-      content: {
-        body: '{{ name }} has entered the raffle!',
-      },
-      routing: {
-        method: 'single',
-        channels: ['inbox'],
-      },
-      data: {
+      body: JSON.stringify({
+        status: 'Not Authorized',
+      }),
+    }
+  } else {
+    // get values from the POST
+    const params = new URLSearchParams(event.body)
+    const phoneNumber = params.get('From')
+    const name = params.get('Body')
+    // create a unique id for this user based on their phone number
+    const userId = createHash('sha3-256').update(phoneNumber).digest('hex')
+    // create a Profile for this user
+    const courier = CourierClient()
+    await courier.mergeProfile({
+      recipientId: userId,
+      profile: {
+        phone_number: phoneNumber,
+        phone_number_verified: true,
         name,
       },
-    },
-  })
-  // Send a reply to the user letting them know we've recieved their entry
-  await courier.send({
-    message: {
-      to: {
-        user_id: userId,
+    })
+    // Add the user to our List
+    await courier.lists.subscribe(process.env.COURIER_LIST_ID, userId)
+    // Send an in-app toast and Inbox notification to this website
+    await courier.send({
+      message: {
+        to: {
+          user_id: process.env.PUBLIC_COURIER_USER,
+        },
+        content: {
+          body: '{{ name }} has entered the raffle!',
+        },
+        routing: {
+          method: 'single',
+          channels: ['inbox'],
+        },
+        data: {
+          name,
+        },
       },
-      content: {
-        body: 'Thank you for entering the Courier raffle! Check out our developer docs to learn more about how Courier works: https://courier.com/docs',
+    })
+    // Send a reply to the user letting them know we've recieved their entry
+    await courier.send({
+      message: {
+        to: {
+          user_id: userId,
+        },
+        content: {
+          body: 'Thank you for entering the Courier raffle! Check out our developer docs to learn more about how Courier works: https://courier.com/docs',
+        },
+        routing: {
+          method: 'single',
+          channels: ['sms'],
+        },
       },
-      routing: {
-        method: 'single',
-        channels: ['sms'],
-      },
-    },
-  })
+    })
 
-  // return a 200 OK to Twilio
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      status: `OK`,
-    }),
+    // return a 200 OK to Twilio
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        status: `OK`,
+      }),
+    }
   }
 }
